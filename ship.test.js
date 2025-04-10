4a490659aee85c2ba90d292dcd01b59fc672033c
Ndzi Nfor <ndzinfor@gmail.com>
2025-04-10 16:33:09 +0100

    write test for Ship class

---
+++ ship.test.js
@@ -0,0 +1,40 @@
+import Ship from './ship.js';
+
+describe('Ship', () => {
+    let ship;
+
+    beforeEach(() => {
+        ship = new Ship(3);
+    });
+
+    test('initializes ship with correct propties', () =>{
+        expect(ship.length).toBe(3);
+        expect(ship.hits).toBe(0);
+    });
+
+    test('hit() increases hit count', () => {
+        ship.hit();
+        expect(ship.hits).toBe(1);
+
+        ship.hit();
+        expect(ship.hits).toBe(2);
+    })
+
+    test('isSunk() returns false when hits < length', () => {
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(false);
+    })
+
+    test('isSunk returns true if hits >= length', () => {
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+
+        // an extra hit shouldn't change the hit status
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+
+    });
+});
